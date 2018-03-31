b1a30784323055a5fe607bcbb31aad75839f950f
grewwc <mamawwc@gmail.com>
2018-03-31 13:15:12 +0800

    2018-03-31

--- plot/test.py
+++ plot/test.py
@@ -1,45 +1,22 @@
-import itertools
-import operator
-import math
-import multiprocessing
-import numpy as np
+from flask import Flask, render_template
+
+main: Flask = Flask(__name__)
+
+main.debug = True
+
+@main.route("/")
+def index():
+    return render_template('inner/main.html')
+
+@main.route("/article_1.html")
+def article_1():
+    return render_template('another_inner/article_1.html')
 
 
-def square(channel):
-    global a
-    channel.send(list(map(lambda x: x**2, a)))
+@main.route("/article_2.html")
+def article_2():
+    return render_template('article_2.html')
 
 
-class Person:
-    def __init__(self, gender):
-        self._gender = gender
-
-    @property
-    def gender(self):
-        return self._gender
-
-
-class Students(Person):
-    def __init__(self, name, gender):
-        super(Students, self).__init__(gender)
-        self._name = name
-
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, m_name):
-        if(m_name != 'wwc129'):
-            print('you are not wwc129') 
-        self._name = m_name 
-    
-    def __repr__(self):
-        return "name : {}, gender: {}".format(self._name, self._gender)
-
-
-p = Students('wwc', 'm')
-
-p.name = 'mama'
-multiprocessing.Value('f', 10)
-print(p)
+if __name__ == '__main__':
+    main.run()
